7df0287ace2745f6e074af263a2d4706089e3790
TezzyBear <teztortsg@gmail.com>
2021-03-16 13:06:18 -0500

    Creacion de estructura de archivos

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Demo</title>
+	<script type="text/javascript" src="lib/api.js"></script>
+</head>
+<body>
+	<script type="text/javascript" src="lib/phaser.min.js"></script>
+	<script type="text/javascript" src="js/init.js"></script>
+</body>
+</html>
---
+++ js/init.js
@@ -0,0 +1,7 @@
+
+
+window.onload = function(){
+	console.log("Empezando!");
+	let game = new Phaser.Game(490,570,Phaser.AUTO);
+
+}
